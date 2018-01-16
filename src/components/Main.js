f29e3b5a9c1ab6e8451717ae205e3c124096f0fe
Noemi Quezada <noemi01@csu.fullerton.edu>
2018-01-15 22:02:11 -0800

    Created the start of the navigation bar

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Main.css';
+import Header from "./header/Header.js";
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="Main">
+        <Header/>
+      </div>
+    );
+  }
+}
+
+export default Main;
---
+++ src/components/header/Header.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './Header.css';
+import Logo from "../../assets/logo.svg";
+
+class Header extends Component {
+  render() {
+    return (
+      <section className="outer-header">
+        <header className="header row">
+          <div className="col-sm-12">
+            <div className="secondary-menu-container">
+              <ul className="menu-secondary">
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Prices</a>
+                </li>
+                <li>
+                  <a href="#">Threading Process</a>
+                </li>
+              </ul>
+            </div>
+            <a href="#">
+              <img src={Logo} alt="Prati's Eyebrow Threading"/>
+            </a>
+            <ul className="utilities">
+              <li className="icon-yelp">
+                <a href="#" title="Yelp"><span><i className="fab fa-yelp"></i></span></a>
+              </li>
+            </ul>
+          </div>
+        </header>
+      </section>
+    );
+  }
+}
+
+export default Header;
