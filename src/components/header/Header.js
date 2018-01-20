7cdbdc290c1f6f98db94fc5a98bce15916a0b92b
Noemi <noemi01@csu.fullerton.edu>
2018-01-20 13:34:10 -0800

    Generated a header where the logo is centered

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,35 +5,55 @@ import Logo from "../../assets/logo.svg";
 class Header extends Component {
   render() {
     return (
-      <section className="outer-header">
-        <header className="header row">
-          <div className="columns col-sm-12">
-            <div className="secondary-menu-container">
-              <ul className="menu-secondary">
-                <li>
-                  <a href="#">Services</a>
-                </li>
-                <li>
-                  <a href="#">Prices</a>
-                </li>
-                <li>
-                  <a href="#">Threading Process</a>
-                </li>
-              </ul>
-            </div>
-            <a className="logo-panel" href="#">
-              <img src={Logo} alt="Prati's Eyebrow Threading"/>
-            </a>
-            <ul className="utilities">
-              <li className="icon-yelp">
-                <a href="#" title="Yelp"><span><i className="fab fa-yelp"></i></span></a>
-              </li>
-            </ul>
+      <div id="header" className="row">
+        <div className="col-sm-10 col-sm-offset-1">
+          <div id="inner-header">
+            <span className="left-menu">
+              <a href="#">Services</a>
+              <a href="#">Prices</a>
+            </span>
+            <a id="logo" href="#"><img src={Logo}/></a>
+            <span className="right-menu">
+              <a href="#">Threading Process</a>
+              <span id="yelp-link"><a href="#" title="Yelp"><span><i className="fab fa-yelp"></i></span></a></span>
+            </span>
+            <span className="right-menu-mobile">
+              <span id="yelp-link"><a href="#" title="Yelp"><span><i className="fab fa-yelp"></i></span></a></span>
+            </span>
           </div>
-        </header>
-      </section>
+        </div>
+      </div>
     );
   }
 }
 
 export default Header;
+
+//
+// <section className="outer-header">
+//   <header className="header row">
+//     <div className="columns col-sm-12">
+//       <div className="secondary-menu-container">
+//         <ul className="menu-secondary">
+//           <li>
+//             <a href="#">Services</a>
+//           </li>
+//           <li>
+//             <a href="#">Prices</a>
+//           </li>
+//           <li>
+//             <a href="#">Threading Process</a>
+//           </li>
+//         </ul>
+//       </div>
+//       <a className="logo-panel" href="#">
+//         <img src={Logo} alt="Prati's Eyebrow Threading"/>
+//       </a>
+//       <ul className="utilities">
+//         <li className="icon-yelp">
+//           <a href="#" title="Yelp"><span><i className="fab fa-yelp"></i></span></a>
+//         </li>
+//       </ul>
+//     </div>
+//   </header>
+// </section>
