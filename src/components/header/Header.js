251964d2db567b4482b4efa65fdf7ba43ba4b7cc
Noemi <noemi01@csu.fullerton.edu>
2018-01-20 13:40:32 -0800

    Removed some comments in the Header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -28,32 +28,3 @@ class Header extends Component {
 }
 
 export default Header;
-
-//
-// <section className="outer-header">
-//   <header className="header row">
-//     <div className="columns col-sm-12">
-//       <div className="secondary-menu-container">
-//         <ul className="menu-secondary">
-//           <li>
-//             <a href="#">Services</a>
-//           </li>
-//           <li>
-//             <a href="#">Prices</a>
-//           </li>
-//           <li>
-//             <a href="#">Threading Process</a>
-//           </li>
-//         </ul>
-//       </div>
-//       <a className="logo-panel" href="#">
-//         <img src={Logo} alt="Prati's Eyebrow Threading"/>
-//       </a>
-//       <ul className="utilities">
-//         <li className="icon-yelp">
-//           <a href="#" title="Yelp"><span><i className="fab fa-yelp"></i></span></a>
-//         </li>
-//       </ul>
-//     </div>
-//   </header>
-// </section>
