473046573ca08e07c9b2d1aa025cf844ca98b9ff
Noemi <noemi01@csu.fullerton.edu>
2018-01-21 22:10:11 -0800

    Removed masonry and styled the grid using flexbox

--- src/components/panels/panel-content/prices/Prices.js
+++ src/components/panels/panel-content/prices/Prices.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Masonry from 'react-masonry-component';
 import './Prices.css';
 
 var children = [
@@ -16,19 +15,6 @@ var children = [
     {category: 'facial', service: 'european facial', cost: 50},
     {category: 'facial', service: 'mini facial', cost: 25},
 ]
-
-var masonryOptions = {
-  // fitWidth: true,
-  transitionDuration: '0.2s',
-  // itemSelector: '.grid-item',
-  columnWidth: '.col-sm-3',
-  // gutter: 'gutter-size',
-  // percentPosition: true,
-
-}
-
-var masonryStyles = {
-}
 
 class Prices extends Component {
   constructor(props) {
@@ -48,6 +34,7 @@ class Prices extends Component {
     } else {
       this.setState({elements: children});
     }
+    var elem = document.querySelector('.grid');
   }
 
   handleOnCategoryClick(e) {
@@ -57,7 +44,7 @@ class Prices extends Component {
   render() {
     var childElements = this.state.elements.map(function(element, key){
     return (
-          <div className="element col-sm-3" key={key}>
+          <div className="grid-item" key={key}>
               <span className="category">{element.category}</span>
               <span className="service">{element.service}</span>
               <span className="price">${element.cost}</span>
@@ -74,15 +61,7 @@ class Prices extends Component {
             <li>Facials</li>
           </ul>
         <div className="grid">
-          <Masonry
-                className={'price-grid'}
-                elementType={'div'}
-                options={masonryOptions}
-                style={masonryStyles}
-                disableImagesLoaded={false}
-                updateOnEachImageLoad={false}>
-                {childElements}
-            </Masonry>
+          {childElements}
         </div>
       </div>
     );
