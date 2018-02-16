2fec8bf3a4c4ba39b70e2dcc241786690b5c18c1
Noemi <noemi01@csu.fullerton.edu>
2018-02-15 19:24:08 -0800

    Removed the Header links and made Footer responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,12 +10,12 @@ class Header extends Component {
         <div className="col-sm-10 col-sm-offset-1">
           <div id="inner-header">
             <span className="left-menu">
-              <a href="javascript:void(0)" id="services">Services</a>
-            <a href="javascript:void(0)" id="prices">Prices</a>
+              {/* <a href="javascript:void(0)" id="services">Services</a>
+            <a href="javascript:void(0)" id="prices">Prices</a> */}
             </span>
             <a id="logo" href="javascript:void(0)"><img src={Logo}/></a>
             <span className="right-menu">
-              <a href="javascript:void(0)" id="threading-process">Threading Process</a>
+              {/* <a href="javascript:void(0)" id="threading-process">Threading Process</a> */}
               <span id="yelp-link"><a href="#" title="Yelp"><span><i className="fab fa-yelp"></i></span></a></span>
             </span>
             <span className="right-menu-mobile">
